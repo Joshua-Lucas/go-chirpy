36c85db2141da9c12d287aeccbcbeeb4310d158a
Joshua Luacs <joshuamlucas91@gmail.com>
2026-02-04 06:43:52 -0500

    refactor: routes for api and admin name space

--- main.go
+++ main.go
@@ -1,27 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (c *apiConfig) metricHandlerServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmtMessage := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, c.fileserverHits.Load())
+	w.Write([]byte(fmtMessage))
+}
+
+func (c *apiConfig) resetMetricsHandlerServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	c.fileserverHits.Store(0)
+	w.Write([]byte(("")))
+}
 
 func main() {
 
-	server := http.Server{}
-
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// Api Routes
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 
 	})
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	// App routes
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
 
-	server.Handler = mux
-
-	server.Addr = ":8080"
+	// Admin Routes
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricHandlerServeHTTP)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetricsHandlerServeHTTP)
 
 	server.ListenAndServe()
 
