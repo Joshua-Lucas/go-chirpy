7985c00a0e1ba4b17c40f5b2e645b8010fbc3fb6
Joshua Luacs <joshuamlucas91@gmail.com>
2026-04-03 20:45:41 -0400

    feat: add jwt creation auth func

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"os"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -29,4 +34,24 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 
 	return true, nil
 
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	mySigningKey := []byte(os.Getenv("JWT_SIGNING_KEY"))
+
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		return "", nil
+	}
+
+	return ss, nil
 }
